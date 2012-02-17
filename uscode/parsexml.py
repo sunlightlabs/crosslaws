0fb45a8575a334eb2636399a988770233d0bc3a4
Thom Neale <twneale@gmail.com>
2012-02-17 13:22:10 -0500

    parsed sections out of titles; more to come

--- uscode/parsexml.py
+++ uscode/parsexml.py
@@ -14,94 +14,150 @@ def titledata(title):
 		return f.read()
 
 
-
-def quotes2text(element, join=''.join):
+def _gettext(element, recurse=True):
 	'''
-	Turn an element like this:
-		'Blah blah <quote>More Blah</quote> blah'
-	into a string like this:
-		'Blah blah "More Blah" blah'
+	Extract text from this element, any child element, and the tail.
 	'''
-	quoted = [c.text for c in element.findall('quote')]
-	text = [element.text]
-	for child in element.getchildren():
-		if child.tag in ['text', 'quote']:
-			text.append(child.text)
+	
+	if element.tag == 'quote':
+		yield '"%s"' % element.text
+	else:
+		yield element.text
 
-	replacements = dict((q, '"%s"' % q) for q in quoted)
-	res = []
-	for t in text:
-		res.append(replacements.get(t, t))
-	return join(res)
+	if recurse:
+		for child in element.getchildren():
+			for t in _gettext(child):
+				yield t
 
-class Notes(object):
-	pass
+	tail = element.tail
+	if tail and tail.strip():
+		yield tail
+
+def gettext(element, recurse=True, join=''.join):
+	return join(_gettext(element, recurse))
+	
 
 		
-
+#----------------------------------------------------------------------------
+#
 class Node(object):
 
 	def __init__(self, element):
 		self.element = element
 		self.find = element.find
 		self.findall = element.findall
-	
+
+	@property
+	def tag(self):
+		'''paragraph, subsection, etc.'''
+		return self.element.tag
+	type_ = tag
+
 	@property
 	def enum(self):
-		e = self.element.find('enum').text
-		return e.strip('.')
-
-	@property
-	def kids(self, types='subsection paragraph subparagraph'.split()):
-		for t in types:
-			kids = self.findall(t)
-			if kids:
-				return map(Node, kids)
-
-	@property
-	def text(self):
-		return quotes2text(self.element)
-
-	@property
-	def inline(self):
-		attrib = self.element.attrib
-		return not attrib['display-inline'] == 'no-display-inline'
-
-
-
-class RootNode(Node):
+		e = self.element.find('enum')
+		if e is not None:
+			return e.text.strip('.')
 
 	@property
 	def header(self):
-		return quotes2text(self.find('header'))
-
+		header = self.find('header')
+		if header is not None:
+			return gettext(header)
+		
 	@property
 	def ref_id(self):
-		return self.element.attrib['ref_id']
-
+		return self.element.attrib['ref-id']
+		
 	@property
 	def id(self):
 		return self.element.attrib['id']
 
 	@property
 	def notes(self):
-		return Notes(self.element.find('usc-notes'))
+		return map(Note, self.element.find('usc-notes'))
 
+	@property
+	def text(self):
+		return gettext(self.element)
+
+	@property
+	def inline(self):
+		attrib = self.element.attrib
+		try:
+			return not attrib['display-inline'] == 'no-display-inline'
+		except KeyError:
+			return False
+		
+
+
+#----------------------------------------------------------------------------
+#
+class Note(Node):
+
+	@property
+	def paragraphs(self):
+		return map(gettext, self.element.findall('note-text'))
+
+
+class Section(Node):
 	
+	def _kids(self, types='text subsection paragraph subparagraph'.split()):
+		kids = self.element.getchildren()
+		if kids is not None:
+			kids = kids[1:]
+			for k in kids:
+				if k.tag in types:
+					yield classes[k.tag](k)
 
-class Section(RootNode):
+	@property
+	def kids(self):
+		return list(self._kids())
+
+	def getnote(self, header_or_tag):
+		for note in self.notes:
+			if note.tag == header_or_tag:
+				return note
+			if note.header == header_or_tag:
+				return note
+
+class Subsection(Section):
+	'''
+	The Subsection won't recurse into its child notes when we get its text.
+	'''
+	@property
+	def text(self):
+		return gettext(self.element, recurse=False)
+
+class Paragraph(Subsection):
 	pass
 
+class Subparagraph(Subsection):
+	pass
 
-class Chapter(RootNode):
+class Text(Subsection):
+	pass
+
+classes = {
+	'section': Section,
+	'subsection': Subsection,
+	'paragraph': Paragraph,
+	'subparagraph': Subparagraph,
+	'text': Text,
+ 	}
+
+#----------------------------------------------------------------------------
+#
+class Chapter(Node):
 
 	@property
 	def sections(self):
 		return map(Section, self.findall('section'))
 
 
-
-class Title(RootNode):
+#----------------------------------------------------------------------------
+#
+class Title(Node):
 	'''
 	Has these top-level elements:
 	- chapter
@@ -139,16 +195,16 @@ class Title(RootNode):
 
 
 			
-	
+
 
 
 
 
 if __name__ == "__main__":
-	t9 = Title(data(9))
+	t9 = Title(titledata(9))
+	nn = t9.chapters[0].sections[1].notes
 	ss = t9.sections
 	sss = ss[9]
+	xx = sss.kids
 	hh = sss.find('header')
-	import xml2dict
-	x2d = xml2dict.xml2dict
 	pdb.set_trace()
