5d591e1c7517efa1ec5622a846dba6e814ad9597
Thom Neale <twneale@gmail.com>
2012-08-13 13:26:57 -0400

    edited download script to use relative paths

--- uscode/download/gpolocator.sh
+++ uscode/download/gpolocator.sh
@@ -1,11 +1,12 @@
-# Download and unzip all uscode files for 2011. 
-DIR=$PWD
-HERE=$PWD
-DEST=$DIR"uscode.house.gov/zip/2011/"
-mkdir -P $DIR
-wget -m -l1 -P $DIR http://uscode.house.gov/zip/2011/
+# Download and unzip all uscode files for 2011.
+# Run it from the uscode folder.
+DIR=$PWD"/data"
+DEST=$DIR"/uscode.house.gov/zip/2011/"
+echo $DIR
+echo $DEST
+#mkdir -P $DIR
+#wget -m -l1 -P $DIR http://uscode.house.gov/zip/2011/
 cd $DEST
 for filename in `ls | xargs`; do unzip $filename; done
 rm `find *.zip | xargs`
-cd $HERE
-
+cd $DIR
