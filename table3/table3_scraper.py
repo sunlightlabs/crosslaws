55496718a7cec7a77bf7bb5d9c3db1bf07e1f469
wpli <wpli@users.noreply.github.com>
2014-06-06 17:03:11 -0400

    removed irrelevant code

--- table3/table3_scraper.py
+++ table3/table3_scraper.py
@@ -6,7 +6,6 @@ from StringIO import StringIO
 import re
 import simplejson
 
-
 # Table Three Scraper http://uscode.house.gov/table3/table3years.htm
 # The scrapers grab the URLs for each year from 1789 to 2011, go one directory down to grab the directory, then go one directory below and grab the whole page.  THIS CODE TAKES A WHILE TO RUN.  It may be better to tweak just for the years you want.  Also, could use some refactoring, e.g. merge some of the functions.
 
@@ -18,8 +17,8 @@ import simplejson
 years = { 1950 }
 
 # for testing purposes, the number of files downloaded can be limited.
-LIMIT_SUBSUBRELEASES = False
-LIMIT = 50
+LIMIT_SUBSUBRELEASES = True
+LIMIT = 5
 
 def mainscraper(content): #function to parse Table 3 website
 	doc = etree.parse(StringIO(content), parser=etree.HTMLParser())
@@ -90,65 +89,6 @@ def add_subsubrelease(url): #function to grab sub, sub page data
 	    sys.exit(1) #bomb out, non-zero return indicates error
 	# print content
 	return url, content
-	
-
-def _process_caption_span( expected_class, caption_span, caption_dict ):
-    #print expected_class
-    #print caption_span.get('class')
-    #print type( caption_span )
-    #assert caption_span.get('class') == expected_class
-
-    text_val = " ".join ( caption_span.itertext() )
-
-    caption_dict[ caption_span.get('class') ] = text_val
-
-def _process_table_content_row( table_content_row ):
-	assert( len( table_content_row) == 5 )
-	row_val_dict = {}
-	_process_caption_span( 'actsection', table_content_row[0], row_val_dict )
-	_process_caption_span( 'statutesatlargepage', table_content_row[1], row_val_dict )
-	_process_caption_span( 'unitedstatescodetitle', table_content_row[2], row_val_dict )
-	_process_caption_span( 'unitedstatescodesection', table_content_row[3], row_val_dict )
-	_process_caption_span( 'unitedstatescodestatus', table_content_row[4], row_val_dict )
-
-	return row_val_dict
-
-def _parse_legislative_changes_page( page ):
-	doc = etree.parse(StringIO(page), parser=etree.HTMLParser())
-	tbl = doc.find('//table')
-	
-	caption_dict = {}
-	
-	caption = tbl[ 1 ]
-	
-	assert caption.tag == 'caption'
-	
-	assert len( caption ) == 6
-	
-	_process_caption_span( 'congress', caption[0], caption_dict )
-	_process_caption_span( 'statutesatlargevolume', caption[1], caption_dict )
-	_process_caption_span( 'textdate', caption[2], caption_dict )
-	_process_caption_span( 'prioract', caption[3], caption_dict )
-	_process_caption_span( 'act', caption[4], caption_dict )
-	_process_caption_span( 'nextact', caption[5], caption_dict )
-	
-	table_header_1 = tbl[3]
-	table_header_2 = tbl[4]
-	
-	#print caption_dict
-	
-	rows = []
-	i = 6
-	while i < len( tbl ) - 1:
-		table_content_row = tbl[i]
-		row_val_dict = _process_table_content_row( table_content_row )
-		rows.append( row_val_dict )
-		    #print row_val_dict
-		i += 1
-
-	caption_dict[ 'rows' ] = rows
-	
-	return caption_dict
 
 def main():
 	dataset = []
